702ecfccfc75809fce58c48e23eb45da27778c7c
Jonkambo <dairrow1@mail.ru>
2023-11-26 18:47:57 +0300

    адаптив хедера и первых 2 секций

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 import requestDialogInit from "./modules/dialog-window"
 import authInit from "./modules/hidden-btn"
+import menuInit from "./modules/toggle-menu"
 
 const init = () => {
     requestDialogInit()
     authInit()
+    menuInit()
 }
 
 init()
