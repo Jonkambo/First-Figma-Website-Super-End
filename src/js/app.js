3ad239ec695c248e52b29011b047633d77662f96
Jonkambo <dairrow1@mail.ru>
2023-11-28 18:03:15 +0300

    add slider, rework auth window, rename some classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,15 @@
 import requestDialogInit from "./modules/dialog-window"
 import authInit from "./modules/hidden-btn"
 import menuInit from "./modules/toggle-menu"
+import authWindowInit from "./modules/dialog-auth"
+import swiper from "./modules/slider"
 
 const init = () => {
     requestDialogInit()
     authInit()
     menuInit()
+    authWindowInit()
+    swiper()
 }
 
 init()
